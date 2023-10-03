f7d19ba0f653a733f2eb6a59acb89a2d37fd7a78
Abbas Ali <abbas.taxi.25.1@mountblue.tech>
2023-10-03 12:04:27 +0530

    add login logout functionalities for a user

--- website/views.py
+++ website/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# django authentication system
+from django.contrib.auth import authenticate, login, logout
+# django messaging system
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+            return redirect('home')
+        else:
+            messages.success(request, "Error logging in, please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home/index.html', {})
+
+
+# def loginUser(request):
+#     pass
+
+
+def logoutUser(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect('home')
+
+
+def registerUser(request):
+    return render(request, 'register/register.html', {})
